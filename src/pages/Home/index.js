388d6aaf32d091e6485b9e13aadeca1d5efb4851
Brandon Phillips <brandon.phillips976@gmail.com>
2021-01-13 02:00:23 -0800

    added header component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../../components/Hero'
+import Header from '../../components/Header'
 import './index.css'
 
 export default function Home() {
     return (
         <>
+            <Header />
             <Hero />
         </>
     )
